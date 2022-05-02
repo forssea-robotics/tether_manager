ac64d2cdc0925aaea552837e05813991e0f11a8a
estellearrc <estelle.arricau@gmail.com>
2022-05-02 18:20:58 +0200

    init ROS2 python package

--- scripts/tether_turn_counter_2.py
+++ scripts/tether_turn_counter_2.py
@@ -4,7 +4,7 @@
 from math import pi, degrees
 from threading import Lock
 
-import rospy
+import rclpy
 from numpy import round
 from std_msgs.msg import Float32
 from std_srvs.srv import Trigger
@@ -17,17 +17,17 @@ from forssea_utilities.Rate import Rate
 
 class TurnCounter(object):
     def __init__(self):
-        rospy.init_node('turn_counter')
+        self.node = rclpy.Node.__init__('turn_counter')
         self.total_turn_count = 0.0
         self.current_heading, self.heading_zero = None, None
         self.lock = Lock()
         self.rate = Rate()
-        self.pub = rospy.Publisher('count_output', Float32, queue_size=1, latch=True)
+        self.pub = self.node.create_publisher(Float32, 'count_output', queue_size=1, latch=True)
         self.pub.publish(self.total_turn_count)
 
-        rospy.Subscriber('odometry/filtered', Odometry, self.on_odom_received)
+        self.node.create_subscription(Odometry, 'odometry/filtered', self.on_odom_received)
 
-        self.reset_server = rospy.Service("reset_counter", Trigger, self.reset)
+        self.reset_server = rclpy.Service("reset_counter", Trigger, self.reset)
 
     def on_odom_received(self, msg):
         """
@@ -37,15 +37,15 @@ class TurnCounter(object):
         self.lock.acquire()
         q = msg.pose.pose.orientation
         _, _, heading = quat2euler([q.w, q.x, q.y, q.z])
-        rospy.logdebug("------received odom with heading: {:.4f}°".format(degrees(heading)))
+        self.node.get_logger().debug("------received odom with heading: {:.4f}°".format(degrees(heading)))
         if self.heading_zero is not None:
             delta = to_angle(heading - self.current_heading)
             turn_inc = delta / (2 * pi)
             self.total_turn_count += turn_inc
-            rospy.logdebug("New increment is: {:.4f}° ({:.4f} turns)".format(degrees(delta), turn_inc))
-            rospy.logdebug("Total turn count: {:.4f} ({:.4f})".format(self.total_turn_count, degrees(self.total_turn_count * 2 * pi)))
+            self.node.get_logger().debug("New increment is: {:.4f}° ({:.4f} turns)".format(degrees(delta), turn_inc))
+            self.node.get_logger().debug("Total turn count: {:.4f} ({:.4f})".format(self.total_turn_count, degrees(self.total_turn_count * 2 * pi)))
         else:
-            rospy.logdebug("initializing Init_heading to: {:.4f}°".format(degrees(heading)))
+            self.node.get_logger().debug("initializing Init_heading to: {:.4f}°".format(degrees(heading)))
             self.heading_zero = heading
         self.current_heading = heading
         self.pub.publish(Float32(self.total_turn_count))
@@ -53,7 +53,7 @@ class TurnCounter(object):
 
     def reset(self, req):
         self.lock.acquire()
-        rospy.logdebug("reset called")
+        self.node.get_logger().debug("reset called")
         self.heading_zero, self.current_heading = None, None
         self.total_turn_count = 0.0
         self.pub.publish(Float32(self.total_turn_count))
@@ -61,7 +61,7 @@ class TurnCounter(object):
         return True, ''
 
     def work(self):
-        while not rospy.is_shutdown():
+        while not rclpy.is_shutdown():
             self.pub.publish(Float32(-self.total_turn_count))
             self.rate.sleep()
 
